868dc27f1edf3d2af2b091bee2a43c28364bae8f
NicholasKoblitz <f9ycn6yfzc@privaterelay.appleid.com>
2023-06-11 12:01:24 +0000

    Started working main game loop

--- TestFightingGame.cpp
+++ TestFightingGame.cpp
@@ -1,6 +1,9 @@
 //#include "test.cpp"
 #include <iostream>
-
+#include <string>
+#include "Player.cpp"
+#include "Bat.cpp"
+#include "Rat.cpp"
 
 using namespace std;
 
@@ -9,6 +12,53 @@ int main()
     // Uncommit to run tests
     //unitTests();
 
+    // Game Variables
+    bool gameOn {1};
+    string playerName;
+    int playerInput{0};
+    
+    cout << "Enter the name of your Player: ";
+    getline(cin,playerName);
+    Player player(playerName);
+    cout <<"\n\n";
+    player.showStats();
 
+    while(gameOn)
+        {
+            cout << "Choose your next action" << endl;
+            cout << "1 - Next Fight" << endl;
+            cout << "2 - Show Stats" << endl;
+            cout << "3 - Show Current Weapon" << endl;
+            cout << "4 - Quit" << endl;
+            cout << "\nEnter the number: ";
+            cin >> playerInput;
+
+            switch(playerInput)
+                {
+                    case 1:
+                        {
+                            cout << "Come back later" << endl;
+                        }
+                    break;
+                    case 2:
+                        {
+                            cout << '\n';
+                            player.showStats();
+                            cout << '\n';
+                        }
+                    break;
+                    case 3:
+                        {
+                            cout << '\n';
+                            player.showCurrentWeapon();
+                            cout << '\n';
+                        }
+                    break;
+                    case 4:
+                        {
+                            gameOn = 0;
+                        }
+                }
+        }
     return 0;
 }
