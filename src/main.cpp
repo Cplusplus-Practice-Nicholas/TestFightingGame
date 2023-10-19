1a1453938abe1dff5be32772bf3d923ad563783d
NicholasKoblitz <68592588+NicholasKoblitz@users.noreply.github.com>
2023-10-19 09:43:17 -0400

    cleaned up code

--- src/main.cpp
+++ src/main.cpp
@@ -24,70 +24,57 @@ int main()
     cout << '\n';
 
     while(gameOn)
-        { 
-            cout << "Choose your next action" << endl;
-            cout << "1 - Next Fight" << endl;
-            cout << "2 - Show Stats" << endl;
-            cout << "3 - Show Current Weapon" << endl;
-            cout << "4 - Quit" << endl;
-            cout << "\nEnter the number: ";
-            cin >> playerInput;
-            system("clear");
-           // system("cls");
-            switch(playerInput)
-                {
-                    case 1:
-                        {
-                            //int *randomValues{Room::generateRoom()};
-                            //if(randomValues[0] == 1)
-                            //{
-                                fight(player);
-                                if(player.getHealth() <= 0)
-                                {
-                                    gameOn = 0;
-                                    //system("clear");
-                                    //system("cls");
-                                    cout << "***********************\n" << endl; 
-                                    cout << "GAME OVER" << endl;
-                                    //cout << "\nFinal Score: " << player.score << endl;
-                                    cout << "\n*********************\n" << endl;
-                                }
-                            //}
-                            //else
-                            //{
-                                //cout << "TEST";
-                            //}
-                            
-                            
-                        }
-                    break;
-                    case 2:
-                        {
-                            cout << '\n';
-                            player.showStats();
-                            cout << '\n';
-                        }
-                    break;
-                    case 3:
-                        {
-                            cout << '\n';
-                            player.showCurrentWeapon();
-                            cout << '\n';
-                        }
-                    break;
-                    case 4:
-                        {
-                            gameOn = 0;
-                        }
-                        break;
-                    default:
-                        {
-                            cout << "\nInvalid Entery. Please enter a valid value.\n" << endl;
-                        }
-                }
-            
-        }
-    return 0;
+    { 
+      cout << "Choose your next action" << endl;
+      cout << "1 - Next Fight" << endl;
+      cout << "2 - Show Stats" << endl;
+      cout << "3 - Show Current Weapon" << endl;
+      cout << "4 - Quit" << endl;
+      cout << "\nEnter the number: ";
+      cin >> playerInput;
+      system("clear");
+      // system("cls");
+      switch(playerInput)
+      {
+         case 1:
+         {
+            fight(player);
+            if(player.getHealth() <= 0)
+            {
+               gameOn = 0;
+               cout << "***********************\n" << endl; 
+               cout << "GAME OVER" << endl;
+               cout << "\n*********************\n" << endl;
+            }
+         {  
+         break;
+         case 2:
+         {
+            cout << '\n';
+            player.showStats();
+            cout << '\n';
+         }
+         break;
+         case 3:
+         {
+            cout << '\n';
+            player.showCurrentWeapon();
+            cout << '\n';
+         }
+         break;
+         case 4:
+         {
+            gameOn = 0;
+         }
+         break;
+         default:
+         {
+            cout << "\nInvalid Entery. Please enter a valid value.\n" << endl;
+         }
+      }
+   }
+   
+   return 0;
 }
 
 
